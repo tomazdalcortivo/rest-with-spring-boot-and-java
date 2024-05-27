3f926bb8fe1503d49fd8e6039bdd25664f8d0dd4
tomazdalcortivo <p12547254@gmail.com>
2024-05-27 09:54:37 -0300

    first setup flyway

---
+++ api-gateway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (first_name, last_name, address, gender)
+VALUES
+  ('John', 'Doe', '123 Main St, Anytown, USA', 'Male'),
+  ('Jane', 'Doe', '456 Elm St, Othertown, USA', 'Female'),
+  ('Alice', 'Smith', '789 Oak St, Anycity, USA', 'Female');
