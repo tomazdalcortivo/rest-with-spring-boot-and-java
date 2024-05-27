fca30791ef7ea667c3282ce8b1e4371206371423
tomazdalcortivo <p12547254@gmail.com>
2024-05-27 15:24:37 -0300

    custom json serialization

--- api-gateway/src/main/java/br/com/tomaz/api_gateway/data/vo/v1/PersonVO.java
+++ api-gateway/src/main/java/br/com/tomaz/api_gateway/data/vo/v1/PersonVO.java
@@ -1,21 +1,28 @@
 package br.com.tomaz.api_gateway.data.vo.v1;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable {
 
 
     private Long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
 
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {
