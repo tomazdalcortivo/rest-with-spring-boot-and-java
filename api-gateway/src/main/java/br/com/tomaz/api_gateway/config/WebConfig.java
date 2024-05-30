860a8c11c551ee620442e9490e2894a953eb6bfc
tomazdalcortivo <p12547254@gmail.com>
2024-05-29 22:58:16 -0300

    CORS global setup

--- api-gateway/src/main/java/br/com/tomaz/api_gateway/config/WebConfig.java
+++ api-gateway/src/main/java/br/com/tomaz/api_gateway/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.tomaz.api_gateway.config;
 
 import br.com.tomaz.api_gateway.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,10 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigin = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+//                .allowedMethods("GET", "POST", "PUT")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigin)
+                .allowCredentials(true);
     }
 
     @Override
